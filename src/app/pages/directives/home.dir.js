40c97d14caca4ef92a0674c6678c485c3a1d334e
dremonkey <a.deutmeyer@gmail.com>
2014-01-29 18:21:47 -0800

    initial commit

---
+++ src/app/pages/directives/home.dir.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('particle.pages.directives', [])
+  .directive('home', function () {
+    var directiveDefinitionObject = {
+
+      priority: 10,
+
+      link: function () {}
+    };
+
+    return directiveDefinitionObject;
+  });
